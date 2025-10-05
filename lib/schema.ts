22cb422cbe23831ede37691126f360d23bedce78
basmlawaled <basmlawaled1@gmail.com>
2025-10-05 03:33:28 +0300

    fix: align form fields with userSchema

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,7 +1,8 @@
 import {z} from "zod"
 export const userSchema = z.object({
     name:z.string()
-    .min(2, "Name is required")
+    email: z.string().email(),
+    .min(2, "Name is required"),
     .max(100, "Maximum is too long"),
     about: z.string().optional(),
     country: z.string().min(1, "Country is required"),
